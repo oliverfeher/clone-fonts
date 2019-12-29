87a0884cb81a72773982206360e395b0e877b8be
oliverfeher <olcsee@gmail.com>
2019-12-29 03:19:39 -0500

    Quick touchup on imgs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let darkLight = document.getElementById("day-night");
 
 darkLight.onclick = function() {
     element.classList.toggle("dark-mode");
-    document.getElementById("reset-button").src = "/images/rotating-arrows-circle2.svg"
-    document.getElementById("layout-toggle").src = "/images/grid2.svg"
+    document.getElementById("reset-button").src = "images/rotating-arrows-circle2.svg"
+    document.getElementById("layout-toggle").src = "images/grid2.svg"
 };
 
