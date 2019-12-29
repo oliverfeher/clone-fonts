31716ad89ea80eaafdb575bb1f86fdf99d1f50cb
oliverfeher <olcsee@gmail.com>
2019-12-29 03:11:34 -0500

    Finish dark/light mode selector
    - Change img source for layout button
    - Change img source for reset button
    - Change body background color black/white
    - Change body color black/white

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Dark mode function*/
+let element = document.body;
+let darkLight = document.getElementById("day-night");
+
+
+darkLight.onclick = function() {
+    element.classList.toggle("dark-mode");
+    document.getElementById("reset-button").src = "/images/rotating-arrows-circle2.svg"
+    document.getElementById("layout-toggle").src = "/images/grid2.svg"
+};
+
