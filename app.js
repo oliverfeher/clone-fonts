1daa0948f689997d13919a2a72524980fb951d5c
oliverfeher <olcsee@gmail.com>
2019-12-31 01:47:29 -0500

    Finish dark-mode button
    - Add extension to darkLight.onclick's function
    - Layout button changes src on onlick of the darkmode
    - Reset button changes src on onclick of the darkmode
    - All elements are completely responsive to the darkmode

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* Dark mode function*/
 let element = document.body;
 let darkLight = document.getElementById("day-night");
+let reset = document.getElementById("reset-button");
+let layout = document.getElementById("layout-toggle")
+
 
 
 darkLight.onclick = function() {
     element.classList.toggle("dark-mode");
-    document.getElementById("reset-button").src = "images/rotating-arrows-circle2.svg"
-    document.getElementById("layout-toggle").src = "images/grid2.svg"
-};
+
+    var img = reset.src;
+    if (img.indexOf('images/rotating-arrows-circle.svg')!=-1) {
+        reset.src  = 'images/rotating-arrows-circle2.svg';
+    }
+     else {
+       reset.src = 'images/rotating-arrows-circle.svg';
+     }
+
+     var img2 = layout.src;
+    if (img2.indexOf('images/grid.svg')!=-1) {
+        layout.src  = 'images/grid2.svg';
+    }
+     else {
+       layout.src = 'images/grid.svg';
+     }
+}
 
