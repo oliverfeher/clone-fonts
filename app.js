4cdbbd9560d0d21dfe7ba6919fd8fd8b5125f6df
oliverfeher <olcsee@gmail.com>
2020-01-01 02:53:28 -0500

    Create function for custom-text change innerHTML
    - Searchbar default text changes tile title innerHTML
    - Still needs work to implement on the rest of them

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let darkLight = document.getElementById("day-night");
 let reset = document.getElementById("reset-button");
 let layout = document.getElementById("layout-toggle");
 let layout2 = "images/grid2.svg"
-let addIcon = document.getElementsByClassName("sizing");
 
 
 darkLight.onclick = function() {
@@ -23,14 +22,33 @@ darkLight.onclick = function() {
     } else {
        layout.src = 'images/grid.svg';
     }
-    
-    var img3 = addIcon.src;
-    if (img3.indexOf("images/add-button.svg")!=-1) {
-        addIcon.src = "images/add-button2.svg";
-    } else {
-        addIcon.src = "images/add-button.svg";
-    }
+
+    for(i = 0; i < addIcon.legth; i++) {
+        img3[i] = "images/add-button.svg"
+    };
+};
+
+function showMsg() {
+    var customText = document.getElementById("custom-text").value;
+    document.getElementById("font-name").innerHTML = customText;
 }
+
+
+
+
+/* Add-button change src 
+var img3 = addIcon.src;
+    if (img3.indexOf('add-button.svg')!=-1) {
+        for(i = 0; i < addIcon.legth; i++) {
+            img3[i] = "images/add-button2.svg"
+        }
+    } else {
+        for(i = 0; i < addIcon.legth; i++) {
+            img3[i] = "images/add-button.svg"
+        }
+     }
+*/
+
 
 layout.onclick = function() {
     var img = layout.src;
