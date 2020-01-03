65e015c5766e2f4297909a6de832fd1e7e53996c
oliverfeher <olcsee@gmail.com>
2020-01-03 02:29:50 -0500

    Create for loop to iterate over tile title class
    - Create for loop
    - Onkeyup showMsg function executes
    - showMsg function changes tile title innerHTML

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ let darkLight = document.getElementById("day-night");
 let reset = document.getElementById("reset-button");
 let layout = document.getElementById("layout-toggle");
 let layout2 = "images/grid2.svg"
+let customTextBar = document.getElementById("custom-text");
 
 
 darkLight.onclick = function() {
     element.classList.toggle("dark-mode");
 
-    var img = reset.src;
+    let img = reset.src;
     if (img.indexOf('images/rotating-arrows-circle.svg')!=-1) {
         reset.src  = 'images/rotating-arrows-circle2.svg';
     } else {
        reset.src = 'images/rotating-arrows-circle.svg';
      }
 
-    var img2 = layout.src;
+    let img2 = layout.src;
     if (img2.indexOf('images/grid.svg')!=-1) {
         layout.src  = 'images/grid2.svg';
     } else {
@@ -28,10 +29,19 @@ darkLight.onclick = function() {
     };
 };
 
+let titleArr = document.getElementsByClassName("tile-title");
+let tileArr = Array.from(titleArr);
+
+
+
 function showMsg() {
-    var customText = document.getElementById("custom-text").value;
-    document.getElementById("font-name").innerHTML = customText;
+    let customText = customTextBar.value;
+    for(let i = 0; i < titleArr.length; i++) {
+        titleArr[i].innerHTML = customText;
+    }
 }
+
+
 
 
 
